f6514fca1aaab7546a502728c4477e9d6bb857ed
elvxk <pradiptaramadan9c@gmail.com>
2025-01-21 12:27:21 +0700

    update(Welcome): move line

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -4,11 +4,11 @@ const Welcome = ({ config }) => {
     return (
         <>
             <Head title="elvxk" />
+            <h1 className="font-bold text-2xl">Hello Jagoan</h1>
             <div className="flex flex-col justify-center items-center w-full min-h-screen">
                 {config.map((item, index) => {
                     return <h1 key={index}>{item.value}</h1>;
                 })}
-                <h1 className="font-bold text-2xl">Hello Jagoan</h1>
             </div>
         </>
     );
